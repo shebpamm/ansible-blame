a8e7f3d5d9591ee0a0a553eb901cf250790db3c7
shebpamm <erik@opcode.xyz>
2023-03-16 23:29:09 +0200

    add clap, basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version = "1.0", author = "Erik Karsten", about = "Snoop who ran ansibles")]
+struct Args {
+    host: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Host is: {}", args.host);
 }
