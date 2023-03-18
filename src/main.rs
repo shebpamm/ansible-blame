c544ee8a3c76cccb0f2d5fd1f935432838e3bf36
shebpamm <erik@opcode.xyz>
2023-03-18 14:15:12 +0200

    basic file & ssh file fetching

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use clap::Parser;
-use std::path::{Path,PathBuf};
+use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
-#[clap(version = "1.0", author = "Erik Karsten", about = "Snoop who ran ansibles")]
+#[clap(
+    version = "1.0",
+    author = "Erik Karsten",
+    about = "Snoop who ran ansibles"
+)]
 struct Args {
     source: String,
 }
@@ -21,13 +25,33 @@ fn determine_source(source: String) -> Source {
     }
 }
 
+fn aquire_data(source: Source) -> Vec<String> {
+    match source {
+        Source::File(path) => {
+            let contents =
+                std::fs::read_to_string(path).expect("Something went wrong reading the file");
+            contents.lines().map(|s| s.to_string()).collect()
+        }
+
+        Source::Remote(host) => {
+            let mut cmd = std::process::Command::new("ssh");
+            cmd.arg(host).arg("cat /var/log/auth.log");
+            let output = cmd.output().expect("failed to execute process");
+            let contents =
+                String::from_utf8(output.stdout).expect("failed to convert output to string");
+            contents.lines().map(|s| s.to_string()).collect()
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
     let local_timezone = chrono::Local::now().offset();
 
-    match determine_source(args.source) {
-        Source::File(path) => println!("File is: {}", path.display()),
-        Source::Remote(host) => println!("Host is: {}", host),
-    }
+    let source = determine_source(args.source);
+
+    let data = aquire_data(source);
+
+    println!("{:?}", data);
 }
